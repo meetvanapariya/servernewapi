86d1e19a938e1fb1fafdfee56d4bc42e103003f4
Meet Vanapariya <Meet.WEB@200oksolutions.org>
2023-08-22 11:07:13 +0530

    change in normal payment

--- index.js
+++ index.js
@@ -13,119 +13,57 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("working");
 });
-app.post("/payment", async (req, res) => {
-  const uuid = uuidv4();
-  let { amount, id, isSubscription, priceId, planId } = req.body;
-  try {
-    const customer = await stripe.customers.create({
-      id: uuid,
-      payment_method: id,
-      invoice_settings: {
-        default_payment_method: id,
-      },
-      email: "meet18@gmail.com",
-    });
-    // if (!isSubscription) {
-    const payment = await stripe.paymentIntents.create({
-      amount,
-      currency: "INR",
-      description: "Test payment INC",
-      payment_method: id,
-      confirm: true,
-      customer: customer.id,
-    });
-    if (payment.status === "requires_action") {
-      try {
-        const subscription = await stripe.subscriptions.create({
-          customer: customer.id,
-          items: [{ plan: priceId }],
-          collection_method: "send_invoice",
-          days_until_due: 7,
-        });
-        res.status(200).json({ success: true, subscription });
-      } catch (e) {
-        console.log(e);
-        res.json({
-          message: "Payment Failed",
-          success: false,
-        });
-      }
-    } else {
-      res.status(400).json({ success: false, error: "Payment failed" });
-    }
-    // } else {
-    // const subscription = await stripe.subscriptions.create({
-    //   customer: customer.id,
-    //   items: [{ price: priceId }],
-    //   collection_method: "send_invoice",
-    //   days_until_due: 7,
-    // });
-    // }
-    // res.json({
-    //   message: "Payment Successful outside",
-    //   success: true,
-    // });
-  } catch (e) {
-    console.log(e);
-    res.json({
-      message: "Payment Failed",
-      success: false,
-    });
-  }
-});
 
 app.post("/pay", async (req, res) => {
   const uuid = uuidv4();
   let { id, priceId, amount } = req.body;
   try {
-    const customer = await stripe.customers.create({
-      id: uuid,
-      email: "testing9@gmail.com",
-      payment_method: id,
-      invoice_settings: {
-        default_payment_method: id,
-      },
-    });
-    if (priceId == "price_1NbJBQAYF8m6Yfq3sQwgg2rV") {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount,
-        currency: "GBP",
-        description: "One time payment",
+    //create dummy customer
+    const customer = await stripe.customers.create(
+      {
+        id: uuid,
+        email: "jack@gmail.com",
         payment_method: id,
-        customer: customer.id,
-        confirm: true,
-        payment_method_types: ["card"],
-        payment_method_options: {
-          card: {
-            request_three_d_secure: "any",
-          },
+        invoice_settings: {
+          default_payment_method: id,
         },
+      },
+      { idempotencyKey: uuid }
+    );
+    //Check if subscription is one time or recurring by id
+    //when you create product from stripe you will get price id
+    if (priceId == "price_1NbJBQAYF8m6Yfq3sQwgg2rV") {
+      //===== This is for one time payment =====
+
+      // Step 1: Create invoice
+      const invoice = await stripe.invoices.create({
+        customer: customer.id,
+        collection_method: "charge_automatically",
       });
+
+      // Step 2: Create invoice items which will add product details this invoice.
+      const invoiceItems = await stripe.invoiceItems.create({
+        customer: customer.id,
+        price: priceId,
+        quantity: 1,
+        invoice: invoice.id,
+      });
+
+      // Step 3:Finalize invoice that way it will change invoice status from draft to open.
+      await stripe.invoices.finalizeInvoice(invoice.id);
+
+      // Step 4: we will retrieve all details to get client secret to confirm payment in frontend.
+      const invoiceData = await stripe.invoices.retrieve(invoice.id);
+      const paymentIntent = await stripe.paymentIntents.retrieve(
+        invoiceData.payment_intent
+      );
+
+      // Get the client secret from the Payment Intent
       const clientSecret = paymentIntent.client_secret;
-      try {
-        //create invoice
-        const invoice = await stripe.invoices.create({
-          customer: paymentIntent.customer,
-          collection_method: "send_invoice",
-          days_until_due: 30,
-        });
-
-        // Create invoice items
-        const invoiceItems = await stripe.invoiceItems.create({
-          customer: paymentIntent.customer,
-          price: priceId,
-          quantity: 1,
-          invoice: invoice.id,
-        });
-
-        await stripe.invoices.finalizeInvoice(invoice.id);
-        await stripe.invoices.pay(invoice.id);
-      } catch (e) {
-        console.log(e);
-      }
-
       res.json({ clientSecret, message: "Payment Initiated" });
     } else {
+      // ==== This is for subscription creation ====
+      //Subscription default generate invoice so we don't need to code for invoice
       const subscription = await stripe.subscriptions.create({
         customer: customer.id,
         items: [{ plan: priceId }],
@@ -140,6 +78,7 @@ app.post("/pay", async (req, res) => {
         },
         expand: ["latest_invoice.payment_intent"],
       });
+
       const clientSecret =
         subscription.latest_invoice.payment_intent.client_secret;
       res.json({ clientSecret, message: "Payment Initiated" });
